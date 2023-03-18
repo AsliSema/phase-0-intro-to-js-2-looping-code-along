2084e45f91d8a9de98d237113a3a6d5a9b72cf45
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-03-18 12:48:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+/* for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+} */
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const subject = "surprise"
+const messages = [];
+function writeCards(array, subject){
+    for(let i = 0; i < array.length; i++){
+        messages[i] = `Thank you, ${array[i]}, for the wonderful ${subject} gift!`
+    }
+    return messages
+}
+
+function countDown(){
+    let number = 10;
+    while(number >= 0){
+        console.log(number);
+        number--
+    }
+}
+
+writeCards(names, subject);
+countDown();
